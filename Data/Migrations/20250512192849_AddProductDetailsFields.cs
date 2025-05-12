8ce2676eb20e3d5e91511ff58a96ab2db39650c6
ST10252746 <st10252746@vcconnect.edu.za>
2025-05-12 21:29:41 +0200

    Added additional details to the Product model - made edits to product create and index views

---
+++ Data/Migrations/20250512192849_AddProductDetailsFields.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PROG7311_POE_PART_2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductDetailsFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Cost",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LifespanYears",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Manufacturer",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "LifespanYears",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Manufacturer",
+                table: "Products");
+        }
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -17,6 +17,15 @@ namespace PROG7311_POE_PART_2.Models
         [DataType(DataType.Date)]
         public DateTime ProductionDate { get; set; }
 
+        public string Description { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Cost { get; set; }
+
+        public int LifespanYears { get; set; }
+        public string Manufacturer { get; set; }
+
+
         // Foreign key
         [ForeignKey("Farmer")]
         public int FarmerId { get; set; }
